b74a4da86954d2f6997405dacc03fdf8429013cc
u4s5 <u4s5@yandex.ru>
2017-04-01 01:31:14 +0300

    Add FibonacciHeap testing

--- src/test/java/domain/DomainInitialStateTest.java
+++ src/test/java/domain/DomainInitialStateTest.java
@@ -1,6 +1,5 @@
 package domain;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
--- src/test/java/fibonacciHeap/FibonacciHeapTest.java
+++ src/test/java/fibonacciHeap/FibonacciHeapTest.java
@@ -1,7 +1,31 @@
 package fibonacciHeap;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FibonacciHeapTest {
 
+    private FibonacciHeap fb;
+    private StringBuilder log;
 
+    @Before
+    public void init() {
+        fb = new FibonacciHeap();
+        log = new StringBuilder();
+
+        fb.add(1, log);
+        fb.add(-5, log);
+        fb.add(4, log);
+
+        fb.extractMin(log);
+        fb.extractMin(log);
+    }
+
+    @Test
+    public void testFibonacciHeap() {
+        assertEquals("Testing log", log.toString(), "012013401356NKLM89AABFKLMGJDDE1256NOPL13KLM89ADE12");
+    }
 
 }
